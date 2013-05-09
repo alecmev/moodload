2343bf08bcdb8ed8c5b1d1a399b669fdf900344d
Olegs Jeremejevs <olegs@jeremejevs.com>
2013-05-09 16:45:20 +0100

    add google analytics

--- chrome/background.js
+++ chrome/background.js
@@ -1,3 +1,14 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-40792111-1']);
+_gaq.push(['_trackPageview']);
+
+var ga = document.createElement('script');
+ga.type = 'text/javascript';
+ga.async = true;
+ga.src = 'https://ssl.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ga, s);
+
 chrome.pageAction.onClicked.addListener(function(tab)
 {
     chrome.tabs.sendMessage(tab.id, false, function(url)
